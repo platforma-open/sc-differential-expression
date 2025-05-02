aabfec04f42194717c8e8cda9aff2796c73efcab
aiturbidemil <aiturbide@milaboratories.com>
2025-05-02 17:35:47 +0200

    first working version

---
+++ model/src/index.ts
@@ -0,0 +1,142 @@
+import type { GraphMakerState } from '@milaboratories/graph-maker';
+import type {
+  InferOutputsType,
+  PFrameHandle,
+  PlDataTableState,
+  PlRef } from '@platforma-sdk/model';
+import {
+  BlockModel,
+  createPFrameForGraphs,
+  createPlDataTable,
+  isPColumn,
+  isPColumnSpec,
+} from '@platforma-sdk/model';
+
+export type UiState = {
+  anchorColumn?: PlRef;
+  graphState: GraphMakerState;
+  tableState: PlDataTableState;
+};
+
+export type BlockArgs = {
+  countsRef?: PlRef;
+  covariateRefs: PlRef[];
+  contrastFactor?: PlRef;
+  denominator?: string;
+  numerator?: string;
+  log2FCThreshold: number;
+  pAdjThreshold: number;
+  title?: string;
+};
+
+export const model = BlockModel.create()
+
+  .withArgs<BlockArgs>({
+    covariateRefs: [],
+    log2FCThreshold: 1,
+    pAdjThreshold: 0.05,
+  })
+
+  .withUiState<UiState>({
+    tableState: {
+      gridState: {},
+      pTableParams: {
+        sorting: [],
+        filters: [],
+      },
+    },
+    graphState: {
+      title: 'Differential gene expression',
+      template: 'dots',
+      currentTab: null,
+    },
+  })
+
+  .argsValid((ctx) => (
+    (ctx.args.numerator !== undefined))
+  && (ctx.args.denominator !== undefined)
+  && (ctx.args.log2FCThreshold !== undefined)
+  && (ctx.args.pAdjThreshold !== undefined),
+  )
+
+  .output('countsOptions', (ctx) =>
+    ctx.resultPool.getOptions((spec) => isPColumnSpec(spec)
+      && spec.name === 'pl7.app/rna-seq/countMatrix' && spec.domain?.['pl7.app/rna-seq/normalized'] === 'false'
+    , { includeNativeLabel: true, addLabelAsSuffix: true }),
+  )
+
+  .output('metadataOptions', (ctx) =>
+    ctx.resultPool.getOptions((spec) => isPColumnSpec(spec) && spec.name === 'pl7.app/metadata'),
+  )
+
+  .output('denominatorOptions', (ctx) => {
+    if (!ctx.args.contrastFactor) return undefined;
+
+    const data = ctx.resultPool.getDataByRef(ctx.args.contrastFactor)?.data;
+
+    // @TODO need a convenient method in API
+    const values = data?.getDataAsJson<Record<string, string>>()?.['data'];
+    if (!values) return undefined;
+
+    return [...new Set(Object.values(values))];
+  })
+
+  .output('topTablePt', (ctx) => {
+    const pCols = ctx.outputs?.resolve('topTablePf')?.getPColumns();
+    if (pCols === undefined) {
+      return undefined;
+    }
+
+    return createPlDataTable(ctx, pCols, ctx.uiState?.tableState);
+  })
+
+  .output('topTablePf', (ctx): PFrameHandle | undefined => {
+    let pCols = ctx.outputs?.resolve('topTablePf')?.getPColumns();
+    if (pCols === undefined) {
+      return undefined;
+    }
+
+    const upstream = ctx.resultPool
+      .getData()
+      .entries.map((v) => v.obj)
+      .filter(isPColumn)
+      .filter((col) => {
+        return col.spec.name === 'pl7.app/rna-seq/geneSymbols';
+      });
+
+    pCols = [...pCols, ...upstream];
+
+    return createPFrameForGraphs(ctx, pCols);
+  })
+
+  .output('anchorSpec', (ctx) => {
+    // return the Reference of the p-column selected as input dataset in Settings
+    if (!ctx.uiState?.anchorColumn) return undefined;
+
+    // Get the specs of that selected p-column
+    const anchorColumn = ctx.resultPool.getPColumnByRef(ctx.uiState?.anchorColumn);
+    const anchorSpec = anchorColumn?.spec;
+    if (!anchorSpec) {
+      console.error('Anchor spec is undefined or is not PColumnSpec', anchorSpec);
+      return undefined;
+    }
+
+    return anchorSpec;
+  })
+
+  .output('isRunning', (ctx) => ctx.outputs?.getIsReadyOrError() === false)
+
+  .sections((_ctx) => ([
+    { type: 'link', href: '/', label: 'Main' },
+    { type: 'link', href: '/volcano', label: 'Volcano plot' },
+  ]))
+
+  .title((ctx) =>
+    ctx.args.title
+      ? `sc Differential Expression - ${ctx.args.title}`
+      : 'sc Differential Expression',
+  )
+
+  .done();
+
+export type BlockOutputs = InferOutputsType<typeof model>;
---
+++ ui/src/pages/MainPage.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import '@milaboratories/graph-maker/styles';
+import { PlAgDataTable, PlAgDataTableToolsPanel, PlNumberField, PlBlockPage, PlBtnGhost, PlDropdownRef, PlMaskIcon24, PlSlideModal, PlRow, PlAlert, PlDropdownMulti, PlDropdown, PlAccordionSection } from '@platforma-sdk/ui-vue';
+import type { PlDataTableSettings } from '@platforma-sdk/ui-vue';
+import { useApp } from '../app';
+import { computed, reactive } from 'vue';
+import type { PlRef } from '@platforma-sdk/model';
+import { plRefsEqual } from '@platforma-sdk/model';
+
+const app = useApp();
+
+const tableSettings = computed<PlDataTableSettings>(() => ({
+  sourceType: 'ptable',
+  pTable: app.model.outputs.topTablePt,
+}));
+
+const data = reactive<{
+  settingsOpen: boolean;
+}>({
+  settingsOpen: app.model.args.countsRef === undefined,
+});
+
+function setInput(inputRef?: PlRef) {
+  app.model.args.countsRef = inputRef;
+  if (inputRef)
+    app.model.args.title = app.model.outputs.countsOptions?.find((o) => plRefsEqual(o.ref, inputRef))?.label;
+  else
+    app.model.args.title = undefined;
+}
+
+const covariateOptions = computed(() => {
+  return app.model.outputs.metadataOptions?.map((v) => ({
+    value: v.ref,
+    label: v.label,
+  })) ?? [];
+});
+
+const contrastFactorOptions = computed(() => {
+  return app.model.args.covariateRefs.map((ref) => ({
+    value: ref,
+    label: covariateOptions.value.find((m) => m.value.name === ref.name)?.label ?? '',
+  }));
+});
+
+const numeratorOptions = computed(() => {
+  return app.model.outputs.denominatorOptions?.map((v) => ({
+    value: v,
+    label: v,
+  }));
+});
+
+const denominatorOptions = computed(() => {
+  return numeratorOptions.value?.filter((op) =>
+    !app.model.args.numerator?.includes(op.value));
+});
+
+</script>
+
+<template>
+  <PlBlockPage>
+    <template #title>sc Differential Expression</template>
+    <template #append>
+      <!-- PlAgDataTableToolsPanel controls showing  Export column and filter-->
+      <PlAgDataTableToolsPanel/>
+      <PlBtnGhost @click.stop="() => data.settingsOpen = true">
+        Settings
+        <template #append>
+          <PlMaskIcon24 name="settings" />
+        </template>
+      </PlBtnGhost>
+    </template>
+    <PlAgDataTable
+      v-model="app.model.ui.tableState"
+      :settings="tableSettings"
+      show-columns-panel
+      show-export-button
+    />
+    <PlSlideModal v-model="data.settingsOpen">
+      <template #title>Settings</template>
+      <PlDropdownRef
+        v-model="app.model.args.countsRef" :options="app.model.outputs.countsOptions"
+        label="Select dataset"
+        clearable @update:model-value="setInput"
+      />
+      <PlDropdownMulti v-model="app.model.args.covariateRefs" :options="covariateOptions" label="Covariates" />
+      <PlDropdown v-model="app.model.args.contrastFactor" :options="contrastFactorOptions" label="Contrast factor" />
+      <PlDropdown v-model="app.model.args.numerator" :options="numeratorOptions" label="Numerator" />
+      <PlDropdown v-model="app.model.args.denominator" :options="denominatorOptions" label="Denominator" />
+
+      <PlAccordionSection label="THRESHOLD PARAMETERS">
+      <PlRow>
+        <PlNumberField
+          v-model="app.model.args.log2FCThreshold"
+            label="Log2(FC)" :minValue="0" :step="0.1"
+          >
+            <template #tooltip>
+              Select a valid absolute log2(FC) threshold for identifying
+              significant genes.
+            </template>
+          </PlNumberField>
+          <PlNumberField
+            v-model="app.model.args.pAdjThreshold"
+            label="Adjusted p-value" :minValue="0" :maxValue="1" :step="0.01"
+          />
+        </PlRow>
+        <!-- Add warnings if selected threshold are out of most commonly used bounds -->
+        <PlAlert v-if="app.model.args.pAdjThreshold > 0.05" type="warn">
+          {{ "Warning: The selected adjusted p-value threshold is higher than the most commonly used 0.05" }}
+        </PlAlert>
+        <PlAlert v-if="app.model.args.log2FCThreshold < 0.6" type="warn">
+          {{ "Warning: The selected Log2(FC) threshold may be too low for most use cases" }}
+        </PlAlert>
+      </PlAccordionSection>
+    </PlSlideModal>
+  </PlBlockPage>
+</template>
